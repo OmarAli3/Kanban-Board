9ac19f92941944dfc17752140c5dc56eb1356e7c
Omar Ali Hassan <omar.ali3898@gmail.com>
2022-10-28 18:05:44 +0200

    Reset file input after reading the file

--- src/components/file-reader/FileReader.tsx
+++ src/components/file-reader/FileReader.tsx
@@ -5,6 +5,9 @@ type JSONFileReaderProps = {
 };
 export default function JSONFileReader(props: JSONFileReaderProps) {
   const { onContentReady } = props;
+  // a state to reset the input value after reading the file
+  const [fileInputKey, setInputKey] = useState(new Date().getTime());
+  const resetInput = () => setInputKey(new Date().getTime());
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -17,6 +20,7 @@ export default function JSONFileReader(props: JSONFileReaderProps) {
     const content = e.target?.result;
     if (!!content) {
       onContentReady(content.toString());
+      resetInput();
     }
   };
 
@@ -30,13 +34,12 @@ export default function JSONFileReader(props: JSONFileReaderProps) {
 
   return (
     <div className="relative overflow-hidden bg-blue-600 text-white rounded-md p-2 shadow-sm hover:shadow-md text-xl font-medium">
-      <button
-        className="flex items-center justify-center focus:outline-none"
-      >
+      <button className="flex items-center justify-center focus:outline-none">
         Import
       </button>
       <input
         type="file"
+        key={fileInputKey}
         accept=".json"
         onChange={handleFileChange}
         className="absolute inset-0 opacity-0 cursor-pointer"
