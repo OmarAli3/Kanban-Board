b2342acf23f1f10911046b4fcc04af87d6673e6d
Omar Ali Hassan <omar.ali3898@gmail.com>
2022-10-28 16:39:39 +0200

    Change settings form style

--- src/components/settings-form/SettingsForm.tsx
+++ src/components/settings-form/SettingsForm.tsx
@@ -49,81 +49,83 @@ export default function SettingsForm(props: SettingsFormProps) {
 
   return (
     <div className="flex justify-center">
-      <form className="flex flex-col gap-4" onSubmit={handleSave}>
-        <p className="pt-4 max-w-[30rem]">
-          Here you can customize your own state machine transitions with each
-          row as the source and each column as the destination.
-          <div className="text-sm text-gray-500">
-            <span className="font-medium">**Note:</span> You can't have a
-            transition from a column to itself because each column is ordered by
-            priority.
+      <div className="overflow-auto">
+        <form className="flex flex-col gap-4" onSubmit={handleSave}>
+          <p className="pt-4 max-w-[30rem]">
+            Here you can customize your own state machine transitions with each
+            row as the source and each column as the destination.
+            <div className="text-sm text-gray-500">
+              <span className="font-medium">**Note:</span> You can't have a
+              transition from a column to itself because each column is ordered
+              by priority.
+            </div>
+          </p>
+          <div
+            className="grid gap-2 items-center rounded-md border border-gray-100 overflow-auto "
+            style={{
+              gridTemplateColumns: `repeat(${
+                Object.values(TaskStatus).length + 1
+              }, minmax(5rem, 1fr))`,
+            }}
+          >
+            <span></span>
+            {Object.values(TaskStatus).map((status, i) =>
+              React.Children.toArray(
+                <HeaderCell className="justify-center">
+                  {getStatusName(status)}
+                </HeaderCell>
+              )
+            )}
+            {Object.values(TaskStatus).map((sourceColumn, i) =>
+              React.Children.toArray(
+                <>
+                  <HeaderCell>{getStatusName(sourceColumn)}</HeaderCell>
+                  {React.Children.toArray(
+                    Object.values(TaskStatus).map((destinationColumn, j) => (
+                      <div className="w-full flex justify-center items-center bg-white">
+                        <input
+                          type="checkbox"
+                          disabled={i === j}
+                          id={destinationColumn}
+                          name={destinationColumn}
+                          value={sourceColumn}
+                          checked={transitionBoardState[
+                            destinationColumn
+                          ].includes(sourceColumn)}
+                          onChange={(e) => {
+                            if (e.target.checked) {
+                              handleSelect(sourceColumn, destinationColumn);
+                            } else {
+                              handleDeselect(sourceColumn, destinationColumn);
+                            }
+                          }}
+                        />
+                      </div>
+                    ))
+                  )}
+                </>
+              )
+            )}
           </div>
-        </p>
-        <div
-          className="grid gap-2 items-center rounded-md border border-gray-100 overflow-auto "
-          style={{
-            gridTemplateColumns: `repeat(${
-              Object.values(TaskStatus).length + 1
-            }, minmax(5rem, 1fr))`,
-          }}
-        >
-          <span></span>
-          {Object.values(TaskStatus).map((status, i) =>
-            React.Children.toArray(
-              <HeaderCell className="justify-center">
-                {getStatusName(status)}
-              </HeaderCell>
-            )
-          )}
-          {Object.values(TaskStatus).map((sourceColumn, i) =>
-            React.Children.toArray(
-              <>
-                <HeaderCell>{getStatusName(sourceColumn)}</HeaderCell>
-                {React.Children.toArray(
-                  Object.values(TaskStatus).map((destinationColumn, j) => (
-                    <div className="w-full flex justify-center items-center bg-white">
-                      <input
-                        type="checkbox"
-                        disabled={i === j}
-                        id={destinationColumn}
-                        name={destinationColumn}
-                        value={sourceColumn}
-                        checked={transitionBoardState[
-                          destinationColumn
-                        ].includes(sourceColumn)}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            handleSelect(sourceColumn, destinationColumn);
-                          } else {
-                            handleDeselect(sourceColumn, destinationColumn);
-                          }
-                        }}
-                      />
-                    </div>
-                  ))
-                )}
-              </>
-            )
-          )}
-        </div>
-        <button
-          type="submit"
-          disabled={
-            JSON.stringify(transitionBoardState) ===
-            JSON.stringify(transitionBoard)
-          }
-          className="bg-blue-600 disabled:bg-gray-400 text-white rounded-md p-1 shadow-sm hover:shadow-md text-lg font-medium"
-        >
-          Save
-        </button>
-        <button
-          type="button"
-          className="bg-white text-gray-700 rounded-md p-1 shadow-sm hover:shadow-md text-lg font-medium"
-          onClick={onCancel}
-        >
-          Cancel
-        </button>
-      </form>
+          <button
+            type="submit"
+            disabled={
+              JSON.stringify(transitionBoardState) ===
+              JSON.stringify(transitionBoard)
+            }
+            className="bg-blue-600 disabled:bg-gray-400 text-white rounded-md p-1 shadow-sm hover:shadow-md text-lg font-medium"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            className="bg-white text-gray-700 rounded-md p-1 shadow-sm hover:shadow-md text-lg font-medium"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
